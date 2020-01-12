1c20cafbe89b3969463a3ff89936a35188fad815
David Birks <david@birks.dev>
2020-01-11 22:55:46 -0500

    Add a nav bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import { 
+  Box,
+  Heading,
+  Button,
+  Flex,
+  Text,
+  Link
+} from 'rebass'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default props =>
+  <>
+    <Flex
+      px={2}
+      color='white'
+      bg='darkblue'
+      alignItems='center'>
+      <Text p={2} fontWeight='bold'>Farese</Text>
+      <Box mx='auto' />
+      <Link variant='nav' href='#!'>
+        Home
+      </Link>
+      <Link variant='nav' href='#!'>
+        About
+      </Link>
+      <Link variant='nav' href='#!'>
+        Map
+      </Link>
+      <Link variant='nav' href='#!'>
+        List
+      </Link>
+      <Link variant='nav' href='#!'>
+        Legacy
+      </Link>
+    </Flex>
+  </>
 
-export default App;
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+//
+//export default App;
