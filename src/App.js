6955fd3d4efca7e2cea1b222d1babba7e18d2ad3
David Birks <david@birks.dev>
2020-01-28 23:53:15 -0500

    Some home page in-progress. Getting started with Rebass

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ThemeProvider } from 'emotion-theming';
+import theme from '@rebass/preset';
 //import logo from './logo.svg';
 //import './App.css';
 import { 
@@ -7,35 +9,127 @@ import {
   Button,
   Flex,
   Text,
-  Link
+  Link,
+  Card
 } from 'rebass'
 
-export default props =>
-  <>
-    <Flex
-      px={2}
-      color='white'
-      bg='darkblue'
-      alignItems='center'>
-      <Text p={2} fontWeight='bold'>Farese</Text>
-      <Box mx='auto' />
-      <Link variant='nav' href='#!'>
-        Home
-      </Link>
-      <Link variant='nav' href='#!'>
-        About
-      </Link>
-      <Link variant='nav' href='#!'>
-        Map
-      </Link>
-      <Link variant='nav' href='#!'>
-        List
-      </Link>
-      <Link variant='nav' href='#!'>
-        Legacy
-      </Link>
-    </Flex>
-  </>
+const App = props => {
+  return(
+    <>
+      <ThemeProvider theme={theme}>
+        <Flex
+          p={2}
+          color='white'
+          bg='primary'
+          alignItems='center'>
+          <Text p={2} fontWeight='bold'>Farese</Text>
+          <Box mx='auto' />
+          <Link
+            variant='nav'
+            href='/'
+            px={2}
+            color='white'>
+            Home
+          </Link>
+          <Link
+            variant='nav'
+            href='/about'
+            px={2}
+            color='white'>
+            About
+          </Link>
+          <Link
+            variant='nav'
+            href='/map'
+            px={2}
+            color='white'>
+            Map
+          </Link>
+          <Link
+            variant='nav'
+            href='/list'
+            px={2}
+            color='white'>
+            List
+          </Link>
+          <Link
+            variant='nav'
+            href='/legacy'
+            px={2}
+            color='white'>
+            Legacy
+          </Link>
+        </Flex>
+
+        <Box
+          p={5}
+          fontSize={4}
+          width={[ 1, 1, 1 ]}
+          color='white'
+          bg='primary'>
+          
+          Welcome to an online directory
+          of Reformed Baptist churches
+
+        </Box>
+
+        <Flex>
+          <Box
+            padding={4}
+            width={1/2}>
+            <Card
+              sx={{
+                p: 3,
+                borderRadius: 16,
+                boxShadow: '0 0 12px rgba(0, 0, 0, .15)',
+              }}>
+              <Box p={3}>
+                <Heading
+                  as='h2'
+                  textAlign='center'>
+                    About
+                </Heading>
+                <Text
+                  p={2}
+                  fontSize={2}>
+                  This directory of Reformed Baptist churches is a collection of 560 churches that hold to the 
+                  1689 London Baptist Confession of Faith or a similar statement of faith. It is our desire that 
+                  this list would be used of God to help Christians find a church when considering a move, or 
+                  when planning a trip.
+                </Text>
+              </Box>
+            </Card>
+          </Box>
+          <Box
+            padding={4}
+            width={1/2}>
+            <Card
+              sx={{
+                p: 3,
+                borderRadius: 16,
+                boxShadow: '0 0 12px rgba(0, 0, 0, .15)',
+              }}>
+              <Box p={3}>
+                <Heading
+                  as='h2'
+                  textAlign='center'>
+                    Quick Disclaimer
+                </Heading>
+                <Text
+                  p={2}
+                  fontSize={2}>
+                  We do not personally know all of these churches nor can we recommend them from personal knowledge.
+                  Some of the churches listed in this directory may not be considered by all to be distinctly Reformed.
+                  However, they would be like-minded in several areas of faith and practice and so are listed on this directory.
+                </Text>
+              </Box>
+            </Card>
+          </Box>
+        </Flex>
+      </ThemeProvider>
+    </>
+  );
+}
 
 //function App() {
 //  return (
@@ -58,4 +152,4 @@ export default props =>
 //  );
 //}
 //
-//export default App;
+export default App;
